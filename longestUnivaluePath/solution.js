ade2cff19114865446cb18df2c9559799450968d
Alex Villarreal <avillarreal01@gmail.com>
2018-04-08 12:41:08 -0400

    refactor

--- longestUnivaluePath/solution.js
+++ longestUnivaluePath/solution.js
@@ -13,14 +13,17 @@ const longestUnivaluePath = function(root) {
 }
 
 const dfs = (node, longestPath) => {
-  let leftMax = node.left === null ? 0 : dfs(node.left, longestPath)
-  let rightMax = node.right === null ? 0 : dfs(node.right, longestPath)
+  let leftPathLength = node.left === null ? 0 : dfs(node.left, longestPath)
+  let rightPathLength = node.right === null ? 0 : dfs(node.right, longestPath)
 
-  let newLeftMax =
-    node.left !== null && node.left.val === node.val ? leftMax + 1 : 0
-  let newRightMax =
-    node.right !== null && node.right.val === node.val ? rightMax + 1 : 0
+  let newLeftPathLength =
+    node.left !== null && node.left.val === node.val ? leftPathLength + 1 : 0
+  let newRightPathLength =
+    node.right !== null && node.right.val === node.val ? rightPathLength + 1 : 0
 
-  longestPath[0] = Math.max(longestPath[0], newLeftMax + newRightMax)
-  return Math.max(newLeftMax, newRightMax)
+  longestPath[0] = Math.max(
+    longestPath[0],
+    newLeftPathLength + newRightPathLength
+  )
+  return Math.max(newLeftPathLength, newRightPathLength)
 }
